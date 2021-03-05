8551e52a367b9d1d222d458cac66c10b12407874
white-l0tus <easruisein@gmail.com>
2021-03-06 02:58:31 +0900

    test

--- src/pages/profiles/ko.svelte
+++ src/pages/profiles/ko.svelte
@@ -1,11 +1,79 @@
-<script>
-var USER_ID = {{ user.id }};
-var USER_NAME = {{ user.name | tojson }};
-var TEAM_ID = {{ user.team_id | tojson }};
-var stats_data = {{ {
-	'type': 'user',
-	'id': user.id,
-	'name': user.name,
-	'account_id': user.id,
-} | tojson }};
+<script lang="ts">
+  import Logo from "./_components/Logo.svelte";
+  import BigLinkButton from './_components/BigLinkButton.svelte';
+  import { metatags } from '@roxi/routify'
+  metatags.title = 'CTF Archive'
+  metatags.description = 'CTF Archive'
 </script>
+
+<main>
+  <header>
+    <div class="banner-logo">
+      <Logo />
+    </div>
+    <section>
+      <h1>시간에 구애받지 않고 즐기는<br />CTF 문제풀이 사이트</h1>
+      <section>
+        대회가 끝나면 더이상 볼 수 없었던 문제들, 모두 안녕!<br />기존에 출제된 문제들을 재현하여 사이트에서 채점받고, 랭킹을 보며 실력을 증진할 수 있습니다.
+      </section>
+    </section>
+  </header>
+</main>
+
+<style>
+  * {
+    text-align: center;
+  }
+
+  main > * {
+    padding: 5rem;
+  }
+
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 20rem;
+    background: url("/assets/images/banner-background.jpg") center center / cover;
+    color: #212121;
+  }
+  .banner-logo {
+    font-size: 4em;
+  }
+
+  header > section {
+    font-size: 1.05em;
+    margin-top: 3em;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1rem 0;
+  }
+  main > section {
+    padding: 3rem 0;
+  }
+  .links {
+    padding: 2rem 0;
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      flex-direction: row;
+    }
+    header > section {
+      margin-top: 0;
+      margin-left: 6em;
+    }
+  }
+
+  @media (min-width: 640px) {
+    .links {
+      flex-direction: row;
+    }
+  }
+</style>
