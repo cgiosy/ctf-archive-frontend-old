d863718f3fb46b1a361911c71a78a8c81a1b3b47
white-l0tus <easruisein@gmail.com>
2021-03-06 02:53:49 +0900

    test

---
+++ src/pages/profiles/ko.svelte
@@ -0,0 +1,11 @@
+<script>
+var USER_ID = {{ user.id }};
+var USER_NAME = {{ user.name | tojson }};
+var TEAM_ID = {{ user.team_id | tojson }};
+var stats_data = {{ {
+	'type': 'user',
+	'id': user.id,
+	'name': user.name,
+	'account_id': user.id,
+} | tojson }};
+</script>
