d5e8208fa27ce90d6a67185be84cf6c626a28d9e
cgiosy <cgiosy@kakao.com>
2021-03-15 20:46:58 +0900

    404 페이지 약간의 수정

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -3,20 +3,26 @@
 </script>
 
 <main>
-  <h1>404 Not Found</h1>
-  <p>요청하신 페이지를 찾을 수 없었어요! ㅜㅜ</p>
-  <div><BigLinkButton href="/">메인 화면으로 이동</BigLinkButton></div>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>요청하신 페이지를 찾을 수 없었어요! ㅜㅜ</p>
+    <BigLinkButton href="/">메인 화면으로 이동</BigLinkButton>
+  </div>
 </main>
 
 <style>
-  main {
+  main,
+  div {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 2rem;
   }
-  main > * {
+  div {
+    width: 100%;
     max-width: var(--content-max-width);
-    margin: 1em 0;
+    padding: 3.5em 1em;
+  }
+  p {
+    margin: 1em 0 2.5em 0;
   }
 </style>
