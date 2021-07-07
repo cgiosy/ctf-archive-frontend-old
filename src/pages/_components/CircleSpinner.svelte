02ab4f37490208fd8d9fdbaa7e9e0bcfaa451a71
cgiosy <cgiosy@kakao.com>
2021-07-07 19:27:08 +0900

    CircleSpinner 컴포넌트 추가

---
+++ src/pages/_components/CircleSpinner.svelte
@@ -0,0 +1,28 @@
+<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+  <circle
+    cx="16"
+    cy="16"
+    r="12.5"
+    fill="none"
+    stroke-dasharray="98.125"
+    stroke-dashoffset="39.27"
+    stroke-width="3"
+  />
+</svg>
+
+<style>
+  svg {
+    width: 32px;
+    height: 32px;
+    animation: spin linear 0.5s infinite;
+    stroke: rgb(var(--link-color));
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0);
+    }
+  }
+</style>
