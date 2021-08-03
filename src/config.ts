509c507614ecba6747596ad34f152a5b89dd64d3
cgiosy <cgiosy@kakao.com>
2021-08-03 23:49:41 +0900

    테스트용 ngrok 주석 추가

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export default {
   requireLogin: false,
   isAllowedPath: (path: string): boolean => ["/intro", "/login", "/signup"].includes(path),
   apiHost: "https://api.ctf-archive.com",
+  // apiHost: "https://953d5fd2be3c.ngrok.io",
 };
