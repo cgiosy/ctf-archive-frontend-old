36bb5034f47b74204caa855e6e6091441e2aef1d
cgiosy <cgiosy@kakao.com>
2021-08-03 23:53:00 +0900

    LevelIcon 코드 개선

--- src/pages/_components/LevelIcon.svelte
+++ src/pages/_components/LevelIcon.svelte
@@ -1,53 +1,31 @@
 <script lang="ts">
-  interface ICategory {
-    index: number;
-    levelSum: number;
-  }
+  import { categoryColors, levelsToCategories, levelsToLevel, reduceMap } from "../../libs/utils";
+  import type { Levels } from "../../types";
 
   export let url: string | undefined = undefined;
-  export let levels: number[] = [0, 0, 0, 0, 0];
+  export let levels: Levels = [0, 0, 0, 0, 0, 0];
   export let solved: boolean = false;
   export let small: boolean = false;
 
-  const getColor = (category: number): string => {
-    return (
-      {
-        0: "rgb(240, 98, 146)",
-        1: "rgb(179, 136, 255)",
-        2: "rgb(255, 112, 67)",
-        3: "rgb(100, 181, 246)",
-        4: "rgb(67, 160, 71)",
-        5: "rgb(97, 97, 97)",
-      }[category] ?? "rgb(--text-color)"
-    );
-  };
-
   const toBorder = (color: string): string => `border: 0.25em solid ${color}`;
 
-  let categories: ICategory[] = [];
-  let levelSum = 0;
-  let expSum = 0;
-  for (let index = 0; index < levels.length; index++) {
-    const level = levels[index];
-    if (level > 0) {
-      levelSum += level;
-      expSum += 1 << level;
-      categories.push({ index, levelSum });
-    }
-  }
+  const categories = levelsToCategories(levels);
+  const { level } = levelsToLevel(levels);
 
-  const deg = 360 / levelSum;
+  const deg = 360 / level;
   const style =
     categories.length === 0
-      ? toBorder("rgb(var(--text-color))")
+      ? "rgb(var(--text-color))"
       : categories.length === 1
-      ? toBorder(getColor(categories[0].index))
-      : `background-image: linear-gradient(rgb(var(--background-color)), rgb(var(--background-color))), conic-gradient(${categories
-          .map(
-            ({ index, levelSum }) =>
-              `${getColor(index)} ${deg * (levelSum - levels[index])}deg ${deg * levelSum}deg`
-          )
-          .join(", ")})`;
+      ? toBorder(categoryColors[categories[0]])
+      : `background-image: linear-gradient(rgb(var(--background-color)), rgb(var(--background-color))), conic-gradient(${reduceMap(
+          categories,
+          (sum, category, index) => [
+            sum + levels[index],
+            `${categoryColors[category]} ${deg * sum}deg ${deg * (sum + levels[index])}deg`,
+          ],
+          0
+        ).join(", ")})`;
 </script>
 
 <a
@@ -55,7 +33,7 @@
   class={`${categories.length >= 2 ? "mixed " : ""}${solved ? "solved " : ""}${
     small ? "small " : ""
   }circle`}
-  {style}>{expSum > 0 ? Math.floor(Math.log2(expSum)) : ""}</a
+  {style}>{level}</a
 >
 
 <style>
