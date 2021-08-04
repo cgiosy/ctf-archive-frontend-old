88889633f2221a83b25b16acd80770107e14aee0
cgiosy <cgiosy@kakao.com>
2021-08-04 20:42:48 +0900

    API 주소 변경 (서브도메인 -> /api)

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   requireLogin: false,
   isAllowedPath: (path: string): boolean => ["/intro", "/login", "/signup"].includes(path),
-  apiHost: "https://api.ctf-archive.com",
+  apiHost: "/api",
   // apiHost: "https://953d5fd2be3c.ngrok.io",
 };
