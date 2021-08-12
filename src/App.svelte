973b1755a5f0b8383691eaf5f2e9e5f76e4d0633
cgiosy <cgiosy@kakao.com>
2021-08-13 07:15:45 +0900

    svelte-query 캐시 persistent 설정

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
   import { Router } from "@roxi/routify";
-  import { QueryClient, QueryClientProvider } from "@sveltestack/svelte-query";
+  import {
+    QueryClient,
+    QueryClientProvider,
+    createLocalStoragePersistor,
+    persistQueryClient,
+  } from "@sveltestack/svelte-query";
   import { routes } from "../.routify/routes";
 
-  const queryClient = new QueryClient();
+  const cacheTime = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        cacheTime,
+      },
+    },
+  });
+
+  const localStoragePersistor = createLocalStoragePersistor();
+
+  persistQueryClient({
+    queryClient,
+    maxAge: cacheTime,
+    persistor: localStoragePersistor,
+  });
 </script>
 
 <QueryClientProvider client={queryClient}>
