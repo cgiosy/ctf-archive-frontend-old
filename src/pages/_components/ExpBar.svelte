1e555d8deaaa7e6489564017a76e602bd097ecfb
cgiosy <cgiosy@kakao.com>
2021-08-14 04:47:41 +0900

    ExpBar 추가

---
+++ src/pages/_components/ExpBar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { expToLevel } from "../../libs/utils";
+
+  export let exp: number = 0;
+  export let height: string = "0.125rem";
+  export let inset: boolean = false;
+
+  const mixedColors = (colors: string[]): string => {
+    const percentage = 100 / colors.length;
+    return `linear-gradient(to right, ${colors
+      .map((color, index) => `${color} ${percentage * (index + 1)}%`)
+      .join(", ")})`;
+  };
+
+  const { level, percentage } = expToLevel(exp);
+  const colors = ["rgb(255, 0, 80)", "rgb(255, 0, 32)"];
+</script>
+
+<div class="bar" style={`height: ${height}; ${false && inset ? "overflow: hidden" : ""}`}>
+  <div
+    class="gauge"
+    style={`width: ${percentage}%;` +
+      `background-image: ${mixedColors(colors)}; ` +
+      `box-shadow: 0 0 0.75rem 0.0625em ${colors[colors.length - 1]}`}
+  />
+  <div class="light" style={`box-shadow: -0.125rem 0 1rem ${colors[colors.length - 1]}`} />
+</div>
+
+<style>
+  .bar {
+    display: flex;
+    width: 100%;
+    background-color: black;
+  }
+  .gauge,
+  .light {
+    display: inline-flex;
+    height: 100%;
+  }
+  .light {
+    width: 0;
+  }
+</style>
