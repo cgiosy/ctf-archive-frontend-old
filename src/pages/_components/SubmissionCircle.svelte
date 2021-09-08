356d5311fe9136c4d97f2217733e0c0acd7a11a0
cgiosy <cgiosy@kakao.com>
2021-09-08 22:10:32 +0900

    제출 원 스타일 변경

--- src/pages/_components/SubmissionCircle.svelte
+++ src/pages/_components/SubmissionCircle.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { categoryColors, setColorOpacity } from "../../libs/utils";
+  import { categoryColors, setColorOpacity, useVars } from "../../libs/utils";
   import { ProblemCategory } from "../../types";
   import type { Levels } from "../../types";
 
@@ -31,6 +31,13 @@
     }
   };
 
+  const initIndex = () => {
+    for (index = 0; index < 6; index++) if (levels[index] !== 0) break;
+    if (index >= 6) index = 0;
+  };
+
+  $: useVars(levels), initIndex();
+
   $: {
     /*
     if (levels.every((level) => 0 <= level && level <= 30 && Number.isInteger(level)))
@@ -50,15 +57,27 @@
     bind:this={inputElm}
     {style}
   />
-  <svg fill="none" viewBox="0 0 192 192">
-    {#each [ProblemCategory.Crypto, ProblemCategory.Reversing, ProblemCategory.Pwnable, ProblemCategory.Misc, ProblemCategory.Forensic, ProblemCategory.Web] as category, i}
+  <svg fill="none" viewBox="0 0 240 240">
+    {#each [0, 10, 20] as level}
+      <circle
+        class="guide"
+        stroke-dasharray="4"
+        stroke-width="1"
+        r={96 + ((level * 1.5) / 30) * 24}
+      />
+    {/each}
+    {#each [ProblemCategory.Pwnable, ProblemCategory.Reversing, ProblemCategory.Crypto, ProblemCategory.Web, ProblemCategory.Forensic, ProblemCategory.Misc] as category, i}
       <circle
         class="{index === category ? 'focused' : ''} {levels[category] > 0 ? 'selected' : ''}"
         stroke={categoryColors[category]}
-        stroke-dashoffset={`${i * 40}%`}
+        stroke-dashoffset={-((i * (144 + ((levels[category] * 2) / 30) * 24)) / 6) * Math.PI}
+        stroke-dasharray="{((144 + ((levels[category] * 2) / 30) * 24) / 6) * Math.PI} {192 *
+          Math.PI}"
+        stroke-width={24 + (levels[category] / 30) * 24}
+        r={72 + (levels[category] / 30) * 24}
         on:click={() => {
           index = category;
-          levels[index] = levels[index] !== 1 ? Math.max(levels[index], 1) : 0;
+          // levels[index] = levels[index] !== 1 ? Math.max(levels[index], 1) : 0;
           inputElm.focus();
         }}
       />
@@ -74,10 +93,10 @@
   input {
     position: absolute;
     display: inline-flex;
-    top: 0.375em;
-    left: 0.375em;
-    width: 2.25em;
-    height: 2.25em;
+    top: 0.825em;
+    left: 0.825em;
+    width: 2.375em;
+    height: 2.375em;
     z-index: 1;
     align-items: center;
     justify-content: center;
@@ -86,7 +105,7 @@
     background: rgb(var(--background-color));
     font-family: Montserrat;
     font-weight: bold;
-    font-size: 4em;
+    font-size: 3em;
     text-align: center;
     flex-shrink: 0;
     transition: transform 0.15s cubic-bezier(0, 0.55, 0.45, 1);
@@ -108,20 +127,28 @@
   circle {
     cx: 50%;
     cy: 50%;
-    r: 37.5%;
-    stroke-width: 12.5%;
-    stroke-dasharray: 40% 200%;
     cursor: pointer;
-    opacity: 0.5;
-    transition: stroke-width 0.05s ease-in-out, opacity 0.1s;
+    opacity: 0.4;
+    transform: rotate(240deg);
+    transform-origin: center;
+    transition: opacity 0.1s;
   }
+  .guide {
+    stroke: rgba(var(--text-color), calc(var(--background-opacity) * 5));
+    cursor: unset;
+    opacity: 1;
+  }
+  /*
   input:focus + svg > circle.focused,
   circle:hover {
-    stroke-width: 25%;
+    stroke-width: 48;
   }
+  */
+  input:focus + svg > circle.focused,
   circle:hover {
     opacity: 0.75;
   }
+  input:focus + svg > circle.selected,
   circle.selected {
     opacity: 1;
   }
