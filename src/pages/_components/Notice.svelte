bde1c48dc5815531575bac979aae89dd5396289f
cgiosy <cgiosy@kakao.com>
2021-09-09 04:25:07 +0900

    Notice margin 추가

--- src/pages/_components/Notice.svelte
+++ src/pages/_components/Notice.svelte
@@ -5,6 +5,7 @@
     font-size: 0.875em;
     background: rgba(var(--link-color), calc(var(--background-opacity) * 1.85));
     padding: 1em 2em;
+    margin: 1em 0.5em;
     border-radius: 0.25em;
     text-align: center;
     word-break: break-all;
