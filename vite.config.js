138ce0a7b00066548b9ce6bf3f1bf8e46dbbd481
cgiosy <cgiosy@kakao.com>
2021-12-27 00:06:15 +0900

    HTML Minifier 추가

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import sveltePreprocess from "svelte-preprocess";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import routify from "@roxi/routify/vite-plugin";
 import { XXHash3 } from "xxhash-addon";
+import htmlMinifier from "html-minifier";
 
 export default defineConfig(() => {
   const production = process.env.NODE_ENV === "production";
@@ -54,14 +55,36 @@ export default defineConfig(() => {
         ],
       }),
       {
-        name: "font-hash-url",
-        transformIndexHtml(html) {
-          return html.replace(/url\(['"]?(.+?\.woff2?)['"]?\)/g, (mat, path) => {
+        name: "hash-font-url",
+        transformIndexHtml: (html) =>
+          html.replace(/url\(['"]?(.+?\.woff2?)['"]?\)/g, (mat, path) => {
             const data = fs.readFileSync(`./public/${path}`);
             const hash = new XXHash3(data).digest().toString("hex").slice(0, 8);
             return mat.replace(path, path + "?v=" + hash);
-          });
-        },
+          }),
+      },
+      {
+        name: "minify-html",
+        transformIndexHtml: (html) =>
+          htmlMinifier.minify(html, {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            maxLineLength: 0,
+            minifyCSS: true,
+            minifyJS: true,
+            processConditionalComments: true,
+            processScripts: ["text/html"],
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            sortAttributes: true,
+            sortClassName: true,
+            trimCustomFragments: true,
+            useShortDoctype: true,
+          }),
       },
     ],
     rollupdedupe: ["svelte"],
