d0c3da0e6337826f2e67d32b8d00c12e3288b5ae
cgiosy <cgiosy@kakao.com>
2022-01-10 21:54:24 +0900

    SideFading 추가

---
+++ src/routes/_components/SideFading.svelte
@@ -0,0 +1,24 @@
+<div><slot /></div>
+
+<style>
+  div {
+    display: inline-block;
+    height: calc(2em + 12px);
+    overflow: hidden;
+    transform: translateZ(0);
+    margin: 1em 0;
+    max-width: 100%;
+  }
+  div::before {
+    content: "";
+    position: fixed;
+    top: -1.5em;
+    left: 0;
+    width: 100%;
+    height: calc(100% + 3em);
+    box-shadow: inset 0 0 2em rgb(var(--background-color)),
+      inset 0 0 2em rgb(var(--background-color)), inset 0 0 2em rgb(var(--background-color)),
+      inset 0 0 2em rgb(var(--background-color)), inset 0 0 2em rgb(var(--background-color));
+    pointer-events: none;
+  }
+</style>
